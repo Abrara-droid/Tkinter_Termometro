14c081e4c185a4828548ac5b2c8aed6b48680e64
Abrara-droid <yunes1991@gmail.com>
2020-03-27 03:36:59 +0100

    He añadido los botones celsius y fahrenheit, cambiado color, colocado todo bien y solucionado errores; Ahora funciona perfecto la parte visual

--- main.py
+++ main.py
@@ -8,16 +8,21 @@ class mainApp(Tk):
     def __init__(self):
         Tk.__init__(self)
         self.title("Termómetro")
-        self.geometry("280x150")
-        self.configure(bg = "#DED789")
+        self.geometry("160x150")
+        self.configure(bg = "#ECECEC")
+        self.resizable(0,0) #Esto no deja redimensionar la ventana
         
         self.temperatura = StringVar(value="") # VV de control
         self.tipoUnidad = StringVar(value= "C")
         
         self.createLayout()
         
-    def createLayout():
-        self.entrada = ttk.Entry(self, variable=self.temperatura)
+    def createLayout(self):
+        self.entrada = ttk.Entry(self, textvariable=self.temperatura).place(x=10, y=10)
+        
+        self.lblUnidad = ttk.Label(self, text="Grados:").place(x=10, y=45)
+        self.unidadF = ttk.Radiobutton(self, text="Fahrenheit", variable=self.tipoUnidad, value="F").place(x=20, y=70)
+        self.unidadC = ttk.Radiobutton(self, text="Celsius", variable=self.tipoUnidad, value="C").place(x=20, y=95)
         
     def start(self):
         self.mainloop()
