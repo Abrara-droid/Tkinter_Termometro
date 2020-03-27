c54eaf7db03c075fae8dd263df49729ecb7ac315
Abrara-droid <yunes1991@gmail.com>
2020-03-27 03:01:06 +0100

    Se ha añadido el recuadro de entrada, hemos metido vv de control

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ from tkinter import *
 from tkinter import ttk
 
 class mainApp(Tk):
+    entrada = None
+    tipoUnidad = None
     
     def __init__(self):
         Tk.__init__(self)
         self.title("Termómetro")
         self.geometry("280x150")
         self.configure(bg = "#DED789")
+        
+        self.temperatura = StringVar(value="") # VV de control
+        self.tipoUnidad = StringVar(value= "C")
+        
+        self.createLayout()
+        
+    def createLayout():
+        self.entrada = ttk.Entry(self, variable=self.temperatura)
         
     def start(self):
         self.mainloop()
